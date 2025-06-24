8ae42da8dc3e0fd2018048cda619d09c2db230d5
Vineet Jassal <vineetjassal35@gmail.com>
2025-06-24 18:38:45 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,11 +55,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/receipt.png"
-        />
-        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
