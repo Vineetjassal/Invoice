483aa08a4540c4d29ad5f076f1302b7c4793bcbd
Vineet Jassal <vineetjassal35@gmail.com>
2025-06-24 18:30:26 +0530

    Updated downloadInvoiceButton.tsx

--- app/component/form/downloadInvoice/downloadInvoiceButton.tsx
+++ app/component/form/downloadInvoice/downloadInvoiceButton.tsx
@@ -172,7 +172,7 @@ export const DownloadInvoiceButton = () => {
             )}
           </Button>
 
-          <p className="text-sm text-gray-500 mb-16">
+          <p className="text-sm text-gray-500 mb-8">
             Your invoice will be downloaded as a PDF file
           </p>
         </div>
